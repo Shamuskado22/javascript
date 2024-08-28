7a2ce86379cd48c63e2737bb998ec64242d286cd
Samuel Almeida <141303008+Shamuskado22@users.noreply.github.com>
2024-08-28 15:23:07 -0300

    ex 008 009 010

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 45
+console.log(`A velocidade do seu veículo é ${vel}Km/h.`)
+if (vel > 45) { //condição simples
+    console.log('Você ultrapassou a velocidade permitida. Receberá uma multa.')
+}
+console.log('Dirija sempre com segurança!')
