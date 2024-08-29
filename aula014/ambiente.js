ad5e4c0cabcfcfd39cfd73d6a2d9d0cda3d77279
Samuel Almeida <141303008+Shamuskado22@users.noreply.github.com>
2024-08-29 16:08:29 -0300

    Create ambiente.js

---
+++ aula014/ambiente.js
@@ -0,0 +1,11 @@
+// var r = 1 
+// while (r <= 7) {
+//     console.log(`Passo ${r}`);
+//     r++
+// }
+
+console.log('Aguarde os Passos...');
+for (var r = 1 ; r <= 7 ; r++) {     // <= Estrutura de repetição
+    console.log(`Passo ${r}`);       //  com variável de controle
+}
+console.log('Carregamento finalizado!');
