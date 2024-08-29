0a8a8d9e1ef429b6ddb152da7e951b505a7b1a94
Samuel Almeida <141303008+Shamuskado22@users.noreply.github.com>
2024-08-29 19:47:47 -0300

    EX JS 003 Finalizado

--- aula014ex/ex016/js/script.js
+++ aula014ex/ex016/js/script.js
@@ -6,5 +6,28 @@ function counter() {
     var end = window.document.getElementById('end')
     var step = window.document.getElementById('step')
     var res = window.document.querySelector('div#res')
-    
+
+    if (start.value.length == 0 || end.value.length == 0 || step.value.length == 0) {
+        window.alert('Preencha os dados')
+        res.innerHTML = 'Não é possível contar'
+    } else {
+        res.innerHTML = `Contagem: `
+        start = Number(start.value)
+        end = Number(end.value)
+        step = Number(step.value)
+        if (step <= 0) {
+            res.innerHTML = 'Valor mínimo de <strong>Passo</strong> é 1'
+        }
+        if (start < end) {
+	        for (var go = start; go <= end; go += step) {
+	            res.innerHTML += `${go} ➡️ `
+	        }
+        } else {
+            for (var go = start; go >= end; go -= step) {
+                res.innerHTML += `${go} ➡️ `
+            }
+        }
+        res.innerHTML += ` 🏆`
+    }
 }
+
