7fb407c52afafd398512c540bbfd3c92f20b7d49
Samuel Almeida <141303008+Shamuskado22@users.noreply.github.com>
2024-08-29 15:20:50 -0300

    Create ambiente.js

---
+++ aula013/ambiente.js
@@ -0,0 +1,21 @@
+var r = 1
+
+do {
+    console.log(`Passo ${r}`)     //<= Estrutura de repetição com
+    r++                           //    teste lógico no final
+} while (r <= 7)
+
+
+// while (r <= 7) {
+//     console.log(`Passo ${r}`)    <= Estrutura de repetição com
+//     r++                              teste lógico no início
+// }
+
+
+// console.log(`Passo ${r}`)
+// console.log(`Passo ${r}`)
+// console.log(`Passo ${r}`)        <=  O mesmo que os dois acima
+// console.log(`Passo ${r}`)             porém mais trabalhoso
+// console.log(`Passo ${r}`)
+// console.log(`Passo ${r}`)
+// console.log(`Passo ${r}`)
