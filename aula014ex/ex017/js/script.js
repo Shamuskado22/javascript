bde1ab2beb3fa14cf5bbb1db394ba491438c4c83
Samuel Almeida <141303008+Shamuskado22@users.noreply.github.com>
2024-08-29 21:25:54 -0300

    EX JS 004 Finalizado

--- aula014ex/ex017/js/script.js
+++ aula014ex/ex017/js/script.js
@@ -1,8 +1,20 @@
 var gene = window.document.getElementById('generate')
 gene.addEventListener('click', generate)
+var option = window.document.getElementsByTagName('option')[0]
+option.style.opacity = '60%'
 
 function generate() {
     var num = window.document.getElementById('num')
-    var res = window.document.querySelector('textarea#tabu')
-    res.innerHTMLText = Number(num.value)
+    var table = window.document.getElementById('table')
+    if (num.value.length == 0) {
+        window.alert('Digite um número!')
+    } else {
+        num = Number(num.value)
+        table.innerHTML = ''
+        for (var calc = 1; calc <= 9; calc++){
+            var item = document.createElement('option')
+            item.text = `${num} x ${calc} = ${num*calc}`
+            table.appendChild(item)
+        }
+    }
 }
